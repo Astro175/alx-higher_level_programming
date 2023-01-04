ffd9053828c9c5ec5ad8b7846f77bc221314d5ff
Astro175 <aspark400@gmail.com>
2023-01-04 06:28:40 +0100

    new

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+def uppercase(string):
+    result = ""
+
+    for c in string:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            result = chr(ord(c) - 32)
+        else:
+            result += c
+    print("{}".format(result))
