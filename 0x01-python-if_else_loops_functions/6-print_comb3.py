ddeaa07b4471247845ad892fa39b012d69297857
Astro175 <aspark400@gmail.com>
2023-01-04 04:41:43 +0100

    new

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        if i < j:
+        if i < j and not (i == 8 and j == 9):
             print("{}, ".format(f"{i:01d}{j:01d}"), end="")
 print("89")
