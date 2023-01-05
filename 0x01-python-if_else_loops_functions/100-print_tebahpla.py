0cd9945377d3674aa573c0c7d059c182aede5915
Astro175 <aspark400@gmail.com>
2023-01-05 05:51:37 +0100

    new

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,15 @@
+#/usr/bin/python3
+# Create a list of lowercase ASCII characters in reverse order
+characters = [chr(i) for i in range(ord('z'), ord('a')-1, -2)]
+
+# Repeat the list twice
+characters = characters * 2
+
+# Join the characters into a single string using the `format` function
+string = '{}'.format(*characters)
+
+# Reverse the string using string slicing
+string = string[::-1]
+
+# Print the string, suppressing the new line at the end
+print(string, end='')
