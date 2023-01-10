e97126206f265c67eba5c24a9e60d3b1e049cb07
Astro175 <aspark400@gmail.com>
2023-01-10 05:14:47 +0100

    new

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,39 @@
+#include <lists.h>
+/**
+ * is_palindrome - Checks if a list is palindrome or not
+ * @head: head node
+ * Return: 1 if palindromel, 0 if not
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	int values[100];
+	int i = 0;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		values[i++] = slow->n;
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	if (fast != NULL)
+	{
+		slow = slow->next;
+	}
+
+	while (slow != NULL)
+	{
+
+		if (values[--i] != slow->n)
+		{
+			return (0);
+		}
+		slow = slow->next;
+	}
+
+	return (1);
+}
+
