7d0bd9fad2edc67d3ce3a4fd7434d0a3418e1d03
Astro175 <aspark400@gmail.com>
2023-01-23 12:51:19 +0100

    task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+
+    for i in range(list_length):
+        try:
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError
+            elif not (isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float))):
+                raise TypeError
+            elif my_list_2[i] == 0:
+                raise ZeroDivisionError
+        except IndexError:
+            print("out of range")
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        finally:
+            result.append(0)
+    return result
