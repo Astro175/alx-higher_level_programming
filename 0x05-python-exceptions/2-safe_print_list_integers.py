f5c3fca8881b516ccfaac7f25acb82100bc7152b
Astro175 <aspark400@gmail.com>
2023-01-23 13:27:23 +0100

    fixed task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             pass
-        i =+ 1
+        i += 1
     print()
     return count
