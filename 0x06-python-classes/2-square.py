c2591f218a2041d0e8a6338e2001f65eeff17e44
Astro175 <aspark400@gmail.com>
2023-01-24 17:49:46 +0100

    done with task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,6 +5,13 @@
 class Square:
     """private attribute"""
     def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
         if not isinstance(size, int):
             raise TypeError("Size must be an integer")
         if size < 0:
