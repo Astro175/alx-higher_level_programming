1fd1224aac64be0925b8e4a232ca7eab3461c83b
Astro175 <aspark400@gmail.com>
2023-01-24 22:55:42 +0100

    fixing bugs in task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,8 +5,18 @@
 class Square:
     """Defines a class that creates squares"""
     def __init__(self, size=0):
-        "initialises an object"""
-        self.size = size
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
 
     @property
     def size(self):
