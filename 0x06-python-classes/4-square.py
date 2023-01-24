1cf221088ee82c82e883baa6ed174507e43d4424
Astro175 <aspark400@gmail.com>
2023-01-24 22:08:32 +0100

    fixed bugs in task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,6 +17,7 @@ class Square:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+        self.__size = size
 
     @property
     def size(self):
@@ -26,7 +27,7 @@ class Square:
     @size.setter
     def size(self, value):
         """sets the size value"""
-        if not ininstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
