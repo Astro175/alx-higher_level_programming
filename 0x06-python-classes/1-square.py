1cd43da4c5bf2061ca7c92d71b70c2b19bc1c03b
Astro175 <aspark400@gmail.com>
2023-01-24 17:20:53 +0100

    done task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square class that creates a private attribute"""
+
+
+class Square:
+    """creates a private attributes"""
+    def __init__(self, size):
+        self.__size = size
