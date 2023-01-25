72ec028910f98b4732f8cef0fb04ef5c87928a57
Astro175 <aspark400@gmail.com>
2023-01-25 05:44:31 +0100

    done with task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Define a module"""
+
+
+class Square:
+    """Creates a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """creating attributes"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """returning size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setting size and value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """returns position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """sets position"""
+        if not isinstance(value, tuple) or len(value) != 2 or \
+        not all(isinstance(i, int) and i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """sets area"""
+        return self.size ** 2
+
+    def my_print(self):
+        """prints attributes"""
+        if self.size == 0:
+            print()
+            return
+        for _ in range(self.position[1]):
+            print()
+        for _ in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
