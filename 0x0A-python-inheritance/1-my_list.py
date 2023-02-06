5f9f153c817db9ff5b11ecde5ce3fcf97e52bd02
Astro175 <aspark400@gmail.com>
2023-02-06 18:10:03 +0100

    fixing bugs in task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,9 +3,13 @@
   Module that inherits a list and prints the element
 """
 
+
 class Mylist(list):
     """
     My list class that inherits list and prints its attributes
     """
     def print_sorted(self):
+        """
+           Method that prints sorted list
+        """
         print(sorted(self))
