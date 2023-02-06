026a655693327dc27ff6f00b8b0b83fab18fb170
Astro175 <aspark400@gmail.com>
2023-02-06 18:32:30 +0100

    some changes to task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that compares an object to a class"""
 
+
 def is_same_class(obj, a_class):
     """function that compares"""
-    return type(obj) == a_class
+    return (type(obj) == a_class)
