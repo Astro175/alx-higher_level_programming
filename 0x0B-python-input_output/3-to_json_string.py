1ffbd00cf82ae5587e6598c1a53e5dee815aef65
Astro175 <aspark400@gmail.com>
2023-02-07 22:52:59 +0100

    done task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+  Module that prints the json representation of a number
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """Function that returns json rep"""
+    return json.dumps(my_obj)
