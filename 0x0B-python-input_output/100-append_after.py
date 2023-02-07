5d8331978820553d4ae8955fde1ef216a171b06f
Astro175 <aspark400@gmail.com>
2023-02-08 00:39:51 +0100

    advanced task 100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+  Module that searches a file for a string,
+  and adds a new string
+"""
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+       function that searches and adds a new_string
+    """
+    with open(filename, "r") as file:
+        for line in file:
+            lines.append(line)
+            if search_string in line:
+                lines.append(new_string + "\n")
+    with open(filename, "w") as w:
+        w.write(lines)
