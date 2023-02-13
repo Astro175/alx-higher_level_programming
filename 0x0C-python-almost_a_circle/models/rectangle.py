4775336fd714242acca0ba72153d49dccad6f64b
Astro175 <aspark400@gmail.com>
2023-02-13 23:00:32 +0100

    done task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Module that creates a rectangle class"""
+
+
+class Rectangle(Base):
+    """class that creates a rectangle model"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """gets the width of the class"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the width of the class"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for height"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """getter for x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter for x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """getter for y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter for y"""
+        self.__y = value
