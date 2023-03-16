3a672932fd30bcae8c796b5c27343d27f51898b7
Astro175 <aspark400@gmail.com>
2023-03-16 12:47:46 +0100

    fixed bug in task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- LIST ALL RECORDS WHERE NAME IS NOT NULL
-SELECT score, name FROM second_table WHERE score IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
