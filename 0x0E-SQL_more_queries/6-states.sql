ce26d30b36903c577c5dc7be86f06a81818041ef
Astro175 <aspark400@gmail.com>
2023-03-16 05:07:50 +0100

    Done task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Script that creates a database and table in it
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	name VARCHAR(256) NOT NULL);
