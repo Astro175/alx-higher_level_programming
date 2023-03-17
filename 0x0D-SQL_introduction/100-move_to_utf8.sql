a5ef536b84c1c16b2e55a1cb9f4a8af617b4d9c9
Astro175 <aspark400@gmail.com>
2023-03-17 14:41:14 +0100

    done task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
---- Script that converts db, table and field to utf-8
+-- Script that converts db, table and field to utf-8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
