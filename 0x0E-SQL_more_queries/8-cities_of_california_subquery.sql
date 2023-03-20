2f310f39f5bb670dee9a46bcc3b69a2c4a3d818d
Astro175 <aspark400@gmail.com>
2023-03-20 14:04:35 +0100

    fixing bugs in task 9

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 --- Script that checks if city and state id are the same
-SELECT  id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
+SELECT  id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id ASC;
