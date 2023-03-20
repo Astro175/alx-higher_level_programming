6f3c0e3bbc1dddc21464484b467f8aa9441fd507
Astro175 <aspark400@gmail.com>
2023-03-20 14:18:23 +0100

    fixing bugs in task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all the cities in a database
 SELECT cities.id, cities.name, states.name
 FROM CITIES
-INNER JOIN ON cities.states_id=states.id
-ORDER BY cities.id;
+INNER JOIN ON cities.state_id=states.id
+ORDER BY cities.id ASC;
