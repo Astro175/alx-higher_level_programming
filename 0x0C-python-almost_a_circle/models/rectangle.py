6ecb5c441e42bf6ad1d9202a85049e1ccf9ad6be
Astro175 <aspark400@gmail.com>
2023-04-06 13:02:10 +0100

    fixed bug in display method description

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,6 +73,6 @@ class Rectangle(Base):
         """Method that returns the area of a rectangle"""
         return self.height * self.width
     def display(self):
-        """Method that prints the rectangle with a #"
+        """Method that prints the rectangle with a #"""
         for _ in range(self.height):
             print("#" * self.width)
