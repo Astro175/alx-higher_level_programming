77faef8547dd9622525b4d58e91697f32b17b384
Astro175 <aspark400@gmail.com>
2023-04-12 05:57:27 +0100

    removed edge case

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,11 +2,8 @@
 
 function add (a, b) {
   console.log(a + b);
-}
 
-if (process.argv.length !== 4) {
-  console.log('Usage: ./add.js <integer1> <integer2>');
-} else if (isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))) {
+if (isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))) {
   console.log('Arguments must be integers');
 } else {
   const a = parseInt(process.argv[2]);
