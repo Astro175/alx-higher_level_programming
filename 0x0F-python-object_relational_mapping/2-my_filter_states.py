3a232553266b7576fbdad914785469bc901648f3
Astro175 <aspark400@gmail.com>
2023-04-17 13:59:30 +0100

    done task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+  Script that takes an argument and queries a database
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    us = sys.argv[1]
+    ps = sys.argv[2]
+    dbs = sys.argv[3]
+    name = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host='localhost', port=3306, user=us, passwd=ps, db=dbs)
+    cursor = db.cursor()
+    cursor.execute(
+            'SELECT * FROM states WHERE name LIKE BINARY \
+    %s ORDER BY id ASC', (name,))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
