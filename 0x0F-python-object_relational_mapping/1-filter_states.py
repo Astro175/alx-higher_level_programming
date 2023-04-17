aecb44af2ab533d1a2759fd7855ef95bedcf26a3
Astro175 <aspark400@gmail.com>
2023-04-17 11:17:15 +0100

    done task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script that selects state with N"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """
+    Connects to the database and makes a query
+    """
+
+    us = sys.argv[1]
+    ps = sys.argv[2]
+    dbs = sys.argv[3]
+
+    db = MySQLdb(
+            host='localhost', port=3306, user=us, passwd=ps, db=dbs)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDEB BY id ASC")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
