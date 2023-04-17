ac264e82104b087fbd108907d9b4f3da837f10af
Astro175 <aspark400@gmail.com>
2023-04-17 22:14:35 +0100

    done task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+  Script that takes an input from user
+  and makes a query to the database
+  but also safe from SQLinjection
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    """Takes an input but safely"""
+    db = MySQLdb.connect(
+            host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+
+    sql_query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+
+    cursor.execute(sql_query, (argv[4] + '%',))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
