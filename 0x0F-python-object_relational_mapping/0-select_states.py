affb356c07cee2873b7669480b8148c34c094d1a
Astro175 <aspark400@gmail.com>
2023-04-17 10:35:28 +0100

    made sure code is only ran when script is executed directly

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,21 +4,20 @@
 """
 import sys
 import MySQLdb
+if __name__ = '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+    db = MySQLdb.connect(host = 'localhost', port = 3306, user = username, passwd = password, db = database)
+    cursor = db.cursor()
 
-db = MySQLdb.connect(host = 'localhost', port = 3306, user = username, passwd = password, db = database)
+    cursor.execute('SELECT * FROM states ORDER BY id ASC')
 
-cursor = db.cursor()
+    rows = cursor.fetchall()
+    
+    for row in rows:
+        print(row)
 
-cursor.execute('SELECT * FROM states ORDER BY id ASC')
-
-rows = cursor.fetchall()
-
-for row in rows:
-    print(row)
-
-cursor.close()
-db.close()
+        cursor.close()
+        db.close()
