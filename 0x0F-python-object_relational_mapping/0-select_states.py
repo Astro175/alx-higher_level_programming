45f411672f0403b76ce12f625a4c9d09288ba527
Astro175 <aspark400@gmail.com>
2023-04-17 10:48:50 +0100

    fixing bugs

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,12 +14,12 @@ if __name__ == '__main__':
             host='localhost', port=3306, user=us, passwd=ps, db=dbs)
     cursor = db.cursor()
 
-    cursor.execute('SELECT * FROM states')
+    cursor.execute('SELECT * FROM states ORDER BY states ASC')
 
     rows = cursor.fetchall()
 
     for row in rows:
         print(row)
 
-        cursor.close()
-        db.close()
+    cursor.close()
+    db.close()
