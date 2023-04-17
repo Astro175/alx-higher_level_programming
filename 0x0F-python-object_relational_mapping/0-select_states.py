9ff04aa970425ead3d600eb972657a65eaee8dc0
Astro175 <aspark400@gmail.com>
2023-04-17 11:43:30 +0100

    fixed pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,17 +6,18 @@ import sys
 import MySQLdb
 if __name__ == '__main__':
     """ Uses a cursor object to query"""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    us = sys.argv[1]
+    ps = sys.argv[2]
+    dbs = sys.argv[3]
 
-    db = MySQLdb.connect(host = 'localhost', port = 3306, user = username, passwd = password, db = database)
+    db = MySQLdb.connect(
+            host='localhost', port=3306, user=us, passwd=ps, db=dbs)
     cursor = db.cursor()
 
     cursor.execute('SELECT * FROM states ORDER BY id ASC')
 
     rows = cursor.fetchall()
-    
+
     for row in rows:
         print(row)
 
