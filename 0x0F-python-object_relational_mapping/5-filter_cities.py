faab88722513b14bb1a5631fcf5dd570a4ba7bf5
Astro175 <aspark400@gmail.com>
2023-04-22 20:09:32 +0100

    done task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+  Script that takes state from user input
+  and lists all cities from that state
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost',
+            port=3306, user=argv[1], passwd=
+            argv[2], db=argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT cities.name FROM cities JOIN \
+            states ON cities.state_id = states.id WHERE \
+            states.name = %s ORDER BY cities.id ASC",
+            (argv[4],))
+
+    rows = fetchall()
+
+    print(", ".join([row[0] for row in rows]))
+    cursor.close()
+    db.close()
