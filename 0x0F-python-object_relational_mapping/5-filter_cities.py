37efdf9a3cb443c2c12d458f9f16d39c6ba69f5b
Astro175 <aspark400@gmail.com>
2023-04-22 20:36:47 +0100

    fixed pycodestyle error

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,15 +9,15 @@ from sys import argv
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost',
-            port=3306, user=argv[1], passwd=
-            argv[2], db=argv[3])
+                         port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
     cursor = db.cursor()
 
     cursor.execute("SELECT cities.id, cities.name FROM cities JOIN \
             states ON cities.state_id = states.id WHERE \
             states.name = %s ORDER BY cities.id ASC",
-            (argv[4],))
+                   (argv[4],))
 
     rows = cursor.fetchall()
     if rows is not None:
