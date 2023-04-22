b3724782dd44d93feb5b2aa6035210c28f57e76c
Astro175 <aspark400@gmail.com>
2023-04-22 15:54:31 +0100

    fixed trailing space

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,11 +9,11 @@ if __name__ == '__main__':
 
     db = MySQLdb.connect(
             host='localhost', port=3306, user=argv[1],
-            passwd=argv[2], db = argv[3])
+            passwd=argv[1], db=argv[3])
     cursor = db.cursor()
 
-    mysql_query = 'SELECT cities.id, cities.name, states.name FROM cities JOIN states \
-    ON cities.state_id = states.id ORDER BY cities.id ASC'
+    mysql_query = 'SELECT cities.id, cities.name, states.name FROM \
+    cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC'
 
     cursor.execute(mysql_query)
 
