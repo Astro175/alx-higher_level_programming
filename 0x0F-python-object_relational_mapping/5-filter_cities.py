30911e1a3ec9d19b9e6b9f5ef09d1e6277ce2431
Astro175 <aspark400@gmail.com>
2023-04-22 20:15:47 +0100

    adjusted query

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT cities.name FROM cities JOIN \
+    cursor.execute("SELECT cities.id, cities.name FROM cities JOIN \
             states ON cities.state_id = states.id WHERE \
             states.name = %s ORDER BY cities.id ASC",
             (argv[4],))
