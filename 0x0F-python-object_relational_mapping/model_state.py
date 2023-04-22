e7ab90aaab803f6235dd069681072f0be179dc42
Astro175 <aspark400@gmail.com>
2023-04-22 23:08:38 +0100

    added states model class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script that creates an ORM table with
+SQLAlchemy"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base
+
+
+class State(Base):
+    """Base model state class"""
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
