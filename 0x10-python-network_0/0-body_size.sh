faefa343cacb4ef7b41d12aa89ea342ce3ad1f60
Astro175 <aspark400@gmail.com>
2023-04-27 17:49:52 +0100

    made script 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-
 # Bash Script that takes in a url and returns the body size
-
-if [ $# -eq 1 ]; then
-
-	if ! echo "$1" | grep -q "://"; then
-
-		url="http://$1"
-
-	else
-
-		url="$1"
-
-	fi
-
-	size=$(curl -s "$url" | wc -c)
-
-	echo "$size"
-
-fi
-
+curl -s "$1" | wc -c
