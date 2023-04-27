ad931f26ea91a57384d4faeb1c083ede1fa11015
Astro175 <aspark400@gmail.com>
2023-04-27 17:45:33 +0100

    done task 0

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Bash Script that takes in a url and returns the body size
+
+if [ $# -eq 1 ]; then
+
+	if ! echo "$1" | grep -q "://"; then
+
+		url="http://$1"
+
+	else
+
+		url="$1"
+
+	fi
+
+	size=$(curl -s "$url" | wc -c)
+
+	echo "$size"
+
+fi
+
