96641268ab65bc81b1986fb9f8c87135609ecfdf
Astro175 <aspark400@gmail.com>
2023-04-28 23:03:32 +0100

    added task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that gets a request header"""
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as feedback:
+    header = feedback.getheader('X-Request-Id')
+
+    print(header)
