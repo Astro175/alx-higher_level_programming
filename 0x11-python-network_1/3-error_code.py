4c693d81387f4f110b0a04cd6e0ebccdca2fcd0e
Astro175 <aspark400@gmail.com>
2023-04-29 04:09:28 +0100

    fixed bug in task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     try:
-        with urllib.request.urlopen(open) as res:
+        with urllib.request.urlopen(url) as res:
             print(res.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print('Error code: {}'.format(e.code))
