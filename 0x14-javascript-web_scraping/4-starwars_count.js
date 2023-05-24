315f43aaa76a8ca9e84c6c62c67cf104b5f3e4cd
astro175 <aspark400@gmail.com>
2023-05-24 14:49:38 +0100

    fixed bugs in task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
-// Script that prints all movies of the 
+// Script that prints all movies of the
 // character Wedge Antilles
 
-url = process.argv[2];
-request = require("request");
+const url = process.argv[2];
+const request = require('request');
 
 request(url, (error, response, body) => {
-	if (error) {
-		console.log(error);
-		return;
-	}
-	movie = JSON.parse(body).results;
-	const wedge = movie.filter(movie => movie.characters.includes(
-		"https://swapi-api.alx-tools.com/api/people/14/"));
-	console.log(wedge.length);
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const movie = JSON.parse(body).results;
+  const wedge = movie.filter(movie => movie.characters.includes(
+    'https://swapi-api.alx-tools.com/api/people/14/'));
+  console.log(wedge.length);
 });
